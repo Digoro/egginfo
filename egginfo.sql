eb229345523ec0abd64c6cc51269d12212797d74
shlee3 <shlee@satreci.com>
2017-08-24 17:35:32 +0900

    egginfo 테이블 생성 sql 추가

---
+++ egginfo.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `egginfo`.`egginfo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `code` VARCHAR(255) NULL,
+  `city` VARCHAR(255) NULL,
+  `farm` VARCHAR(255) NULL,
+  `address` VARCHAR(255) NULL,
+  `certification` VARCHAR(255) NULL,
+  `test_agency` VARCHAR(255) NULL,
+  `grabbed_sample_date` VARCHAR(255) NULL,
+  `pesticide_name` VARCHAR(255) NULL,
+  `pesticide_amount` VARCHAR(255) NULL,
+  `standard` VARCHAR(255) NULL,
+  `location` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
