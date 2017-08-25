ac149e2907e64634a231d06a3c9d49ac9a368bd6
unknown <unknown@unknown.com>
2017-08-26 04:43:47 +0900

    디자인 수정, 조회 --> 지도 마커 반영

--- public/js/eggInfo.js
+++ public/js/eggInfo.js
@@ -3,7 +3,33 @@ app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
 app.controller('eggInfoCtrl', function ($scope, $http) {
+    var map;
+    function setMarker(res) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: new google.maps.LatLng(36.445314, 127.897339),
+            mapTypeId: google.maps.MapTypeId.ROEADMAP
+        });
+        res.forEach(function (v) {
+            let location = [v.address, JSON.parse(v.location).lat, JSON.parse(v.location).lng];
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(location[1], location[2]),
+                map: map
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                var infoWindow = new google.maps.InfoWindow({
+                    content: `<tag>${v.code}</tag> ${location[0]}`
+                });
+                infoWindow.open(map, marker);
+                setTimeout(function () {
+                    marker.hideInfoWindow();
+                }, 3000)
+            })
+        });
+    }
+    var total;
     $http.get('/api/eggInfoList').then(function (res) {
+        total = res.data;
         res.data.map(function (v) {
             v.code = v.code.split(',');
             return v
@@ -14,28 +40,16 @@ app.controller('eggInfoCtrl', function ($scope, $http) {
             center: new google.maps.LatLng(36.445314, 127.897339),
             mapTypeId: google.maps.MapTypeId.ROEADMAP
         });
-        res.data.forEach(function (v) {
-            let location = [v.address, JSON.parse(v.location).lat, JSON.parse(v.location).lng];
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(location[1], location[2]),
-                map: map
-            });
-            google.maps.event.addListener(marker, 'click', function () {
-                var infoWindow = new google.maps.InfoWindow({
-                    content: location[0]
-                });
-                infoWindow.open(map, marker);
-                setTimeout(function () {
-                    marker.hideInfoWindow();
-                }, 3000)
-            })
-        });
-    })
+        setMarker(res.data);
+    });
     $scope.showDetail = function (egg) {
         $('#detailModal').modal('show');
         $scope.eggInfo = egg;
-        console.log(egg)
     };
+    $scope.search = function (searchKey) {
+        if(!searchKey) setMarker(total);
+        else setMarker($scope.filteredEggs);
+    }
 });
 app.filter('setImg', function () {
     return function (item, index) {
@@ -43,6 +57,18 @@ app.filter('setImg', function () {
         return `egg${number}.png`;
     }
 });
+app.directive('krInput', ['$parse', function($parse) {
+    return {
+        priority : 1,
+        restrict : 'A',
+        compile : function(element) {
+            element.on('compositionstart', function(e) {
+                e.stopImmediatePropagation();
+            });
+        },
+    };
+}]);
+
 $( document ).ready(function() {
     $('.trigger').on('click', function() {
         $('.modal-wrapper').toggleClass('open');
