68618296bf1ab7ec12efa2e1d4afa315bc7ed8ac
unknown <unknown@unknown.com>
2017-08-27 18:41:20 +0900

    copyleft 추가

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var updateEggInfo = require('./common').updateEggInfo;
 var index = require('./routes/index');
 var api = require('./routes/api');
 
